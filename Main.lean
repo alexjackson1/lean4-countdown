080ca3ab8a105400aa6e76e31ac9a14c78e26d99
Alex Jackson <mail@alexjackson.uk>
2024-09-28 03:35:43 +0100

    Initial commit

---
+++ Main.lean
@@ -0,0 +1,4 @@
+import Cdown
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
---
+++ lakefile.lean
@@ -0,0 +1,12 @@
+import Lake
+open Lake DSL
+
+package "cdown" where
+  -- add package configuration options here
+
+lean_lib «Cdown» where
+  -- add library configuration options here
+
+@[default_target]
+lean_exe "cdown" where
+  root := `Main
